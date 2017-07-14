03ab2c13f40850b56bb71ccba1c6d1d7d8d57012
Jason Rowland <jasonrowland86>
2017-07-14 13:40:50 -0400

    Made new js file to reorganize my code and reached functionality

--- second.js
+++ second.js
@@ -32,7 +32,7 @@ function createGameBoard() {
   for(let i = 1; i <= 4; i++){
      let newDiv = document.createElement('div');
      newDiv.id = i;
-     newDiv.className = 'box box' + i;
+     newDiv.className = 'box box'+i;
      container.appendChild(newDiv);
   }
     document.getElementById('h1').innerText = "SIMON";
@@ -41,76 +41,62 @@ function createGameBoard() {
 }
 createGameBoard()
 
-
-//****** function that runs game ******
 function playGame() {
-  console.log('playGame');
-  if(turn === 1){
+  document.getElementById('start').innerText = "Reset";
+  document.getElementById('start').removeEventListener('click', playGame);
+  document.getElementById('start').addEventListener('click', reset);
+  computerTurn();
+}
+
+function computerTurn() {
     computerArr.push(Math.floor((Math.random() * 4) + 1));
     console.log(computerArr);
     // computerArr.forEach(animateSequence);
-    turn = 0;
-  } else {
-      console.log('switched to player')
-      for(let i=0; i<boxes.length; i++) {
-        boxes[i].addEventListener('click', playerTurn);
-        if(checkPlayerTurn() && playerArr.length === computerArr.length) {
-          for(let i=0; i<boxes.length; i++) {
-            boxes[i].removeEventListener('click', playerTurn);
-          }
-          level++;
-          score + 10;
-          turn = 1;
-        } else {
-          //animate board to shake on error
-          document.getElementById('h1').innerText = 'GAME OVER'
-          document.getElementById('score').innerText = `Final ${score}`;
-          document.getElementById('start').innerHTML =
-          '<button id = button>Play Again<button>';
-          document.getElementById("start").addEventListener('click', createGameBoard);
-        }
-      }
-    }
+    playerTurn();
 }
 
-//****** Player turn ******
 function playerTurn() {
-    //on click change class and animate
-    playerArr.push(this.getAttribute('id'));
-    checkPlayerTurn();
+  for(let i=0; i<boxes.length; i++) {
+    boxes[i].addEventListener('click', playerMove);
+  }
 }
 
-//****** Check player turn ******
+function playerMove() {
+  playerArr.push(this.getAttribute('id'));
+  console.log(playerArr);
+  if(checkPlayerTurn() === true && playerArr.length === computerArr.length) {
+    playerArr = [];
+    computerTurn();
+  } else if(checkPlayerTurn() === false) {
+    //animate board to shake on error
+    document.getElementById('h1').innerText = 'GAME OVER'
+    document.getElementById('score').innerText = `Final ${score}`;
+    document.getElementById('start').innerHTML = 'Play Again';
+  }
+}
+
 function checkPlayerTurn() {
   let result = playerArr.equals(computerArr);
   return result;
   console.log(result);
 }
-checkPlayerTurn()
 
-//Play again
-function playAgain() {
+function reset() {
   container.innerHTML = '';
   playerArr = [];
-  computerArr = []; //might not need this line if arr is updated in computer turn function
+  computerArr = [];
   //possibly set time out to fix reload bug
   createGameBoard();
 }
+  //Enable user interaction with the board, and register any clicks on the Simon divs
 
-//Animations
-function animateSequence() {
-  //for each loop to call light function on each item in the computerArr
-}
+  //While the player has not entered an incorrect response, and the number of clicks
+  //is less than the length of the sequence, wait for player input
 
-//functions to add afte mvp
-function shakeGameBoard() {
-  //animation to shake game board on game over
-}
-//Score multiplier
-function multiplyScore() {
+//continue adding round until player fails
 
-}
-
+  //alert GAME OVER
+  //add restart button
 
 
 });
